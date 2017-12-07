0c1e43e4ec26db901d10ecf973065320f8f37ccc
egalera <egalera01@gmail.com>
2017-12-07 09:10:02 -0800

    Update Game.java

--- Game.java
+++ Game.java
@@ -10,12 +10,12 @@ public class Game
     public game()
     {
         int playIn=0;
+        int Ending = 3
         // playIn = UserInput.getInt();
         boolean isAlive = true;
         AdventureBegin AdventureBeginObject = new AdventureBegin();
         AdventureBegin.adventureBegin();
-        switch (playIn)
-        {
+       
           public scene1()
           {
               System.out.println("Congratulations on selecting your University.");
@@ -25,10 +25,29 @@ public class Game
               
               System.out.println("Tip off starts and you receive the ball. What do you do?");
              
+              System.out.println("[1] Pass the ball to another teammate");
+              System.out.println("[2] Pop the 3 pointer");
+              System.out.println("[3] Drive in to the rim");
               
+              //playIn = UserInput.getInt();
+              switch (playIn) 
+              {
+                  case 1:
+                      System.out.println("You pass the ball to a teammate and he successfully scores.")
+                          
+                  case 2: 
+                      System.out.println("You shoot the 3 pointer and it misses.")
+                      (Ending --);
+                  case 3: 
+                      System.out.println("You drive in to the rim and the opposing defender pokes the ball out of your hand");
+                      System.out.println("Resulting in a turnover.")
+                      (Ending --);
+              }
               
+         }
         
-          
-        }
+        public scene2
+        {
+            
     }
 }
