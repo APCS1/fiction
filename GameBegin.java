667341d9e185cd6e81e7781b8e6e6cab61973cfb
skysavior <34248891+skysavior@users.noreply.github.com>
2017-12-08 11:42:12 -0800

    Update GameBegin.java

--- GameBegin.java
+++ GameBegin.java
@@ -1,24 +1,26 @@
 
-/**
- * Write a description of class AdventureBegin here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
 public class GameBegin
 {
     public static void gameBegin()
  
     {
+        int playIn= 0;
+        
         System.out.println("Congradulations! you're a successful Highschool basketball prospect being scouted by multiple colleges");
         System.out.println("Many respected univiersities would love to have you represent them");
         System.out.println("Whose application would you like to fill out?");
+        System.out.println();
         
         System.out.println("[1] Kentucky University");
         System.out.println("[2] Duke University");
         System.out.println("[3] UCLA");
         System.out.println("[4] University of North Carolina");
         System.out.println("[5] University of Kansas");
+        
+        playIn = UserInput.getInt();
+        
+        System.out.println("You get denied, you're going to Backupsmore public university instead. ");
+        System.out.println();
     }    
 
 }
