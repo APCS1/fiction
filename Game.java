df44878a954657b62a6c353f44b0576168008565
egalera <egalera01@gmail.com>
2017-12-08 11:55:53 -0800

    Update Game.java

--- Game.java
+++ Game.java
@@ -11,82 +11,77 @@ public class Game
         * Again real sorry
         */
         
+        GameBegin GameBeginObject = new GameBegin();
         GameBegin.gameBegin();
         int playIn = 0;
         int ending = 3;
         
         //Start of scene 1
         
+        System.out.println("Congratulations on selecting your University!");
         System.out.println("Upon entering your college you're generously given a starting position.");
         System.out.println("A couple months has passed and you have gotten comfortable of how things operate.");
         System.out.println("It's mid-way through the season and you're playing against a rival college.");
-         System.out.println();
-              
+        System.out.println();      
         System.out.println("Tip off starts and you receive the ball. What do you do?");
-         System.out.println();
-             
+        System.out.println();     
         System.out.println("[1] Pass the ball to another teammate");
         System.out.println("[2] Pop the 3 pointer");
         System.out.println("[3] Drive in to the rim");
-         System.out.println();
               
-        playIn = UserInput.getInt(); 
-        System.out.println();
+        playIn = UserInput.getInt();
         switch (playIn) 
         {
           case 1:
             System.out.println("You pass the ball to a teammate and he successfully scores.");
-                          
+            break;             
           case 2: 
             System.out.println("You shoot the 3 pointer and it misses.");
             ending--;
+            break;
           case 3: 
             System.out.println("You drive in to the rim and the opposing defender pokes the ball out of your hand");
             System.out.println("Resulting in a turnover.");
             ending --;
+            break;
+               
         }
-         System.out.println();
         
         //Start of scene 2
         
         System.out.println("Halfway through the game, the other team is on the fastbreak.");
         System.out.println("You happen to be the only defender in place.");
-         System.out.println();
         
         System.out.println("How do you anticipate the other team?");
-         System.out.println();
         
         System.out.println("[1] Try and predict the pass to get a steal");
         System.out.println("[2] Play it safe and protect the rim");
-         System.out.println();
         
         playIn = UserInput.getInt();
         switch (playIn)
         {
           case 1:
             System.out.println("You try and predict the pass and you successfully steal the ball and score on the fastbreak.");
-                
+            break; 
           case 2:
             System.out.println("You attempt to protect the rim but you're overpowered by the other players' talents.");
             ending--;
+            break;
         }
-         System.out.println();
         
         //Start of scene 3
         
         System.out.println("It's come to the final seconds of your qame.");
         System.out.println("There's 5 seconds on the clock and you have the chance to win it for your team.");
         System.out.println("You have the ball only to be thinking about the game being on the line.");
-         System.out.println();
-        
-        System.out.println("What are your final choices?");
-         System.out.println();
+        System.out.println();
+        System.out.println();
+        System.outprintln("What are your final choices?");
         
         System.out.println("[1] Shoot the game winner from half court at where you currently stand with the ball.");
         System.out.println("[2] Move up a litte bit and try to shoot deep 3 over defender.");
         System.out.println("[3] Pass the ball to another player to potentially shoot their own game winner.");
         System.out.println("[4] Put on a show with your dribbles and drive to the rim quickly.");
-         System.out.println();
         
         playIn = UserInput.getInt();
         switch (playIn)
@@ -106,10 +101,9 @@ public class Game
              System.out.println("You dribble and actually make your defender slip, but time runs out.");
              ending--;
         }
-         System.out.println();
         
         //Start of ending
-        if (ending == 0) 
+        if (ending == 0)
         {
             System.out.println("Your performance in this game has set the tone for the rest of your season.");
             System.out.println("You do not exceed at any aspect of the game and continue to play as a casual individual.");
@@ -117,14 +111,13 @@ public class Game
             System.out.println("Upon joining this team, you recieve no playing time and go unoticed");
             System.out.println("eventually leading to your removal from the league.");
         }
-        else if (ending > 0) 
+        else if (ending > 1 && == 2)
         {
             System.out.println("Your performance in this game has set the tone for the rest of your season.");
             System.out.println("You have been noticed by many recruiters and projected to be a lottery pick in the upcoming draft.");
             System.out.println("It is now draft day and you were the first round pick being drafted to the 76ers.");
             System.out.println("Upon joining this team you have been granted starting position yet again and carried");
             System.out.println("your team to the NBA Championships eventually obtaining your firts ring your rookie year.");
-        }
-         System.out.println();
+        }   
     }
 }
